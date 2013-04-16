7de33623e86ae5117459407d9f5655a1408bb110
jeanmahai <mahai_1986@126.com>
2013-04-16 21:24:15 +0800

    add files

---
+++ javascript/javascriptExtends/js/array.prototype.remove.js
@@ -0,0 +1,22 @@
+﻿if (!Array.prototype.remove) {
+    /*删除,包含一个入参{function},function返回null表示需要删除,否则不删除*/
+    Array.prototype.remove = function (fn) {
+        if (fn) {
+            for (var i = 0; i < this.length; i++) {
+                if (fn(this[i]) == null) {
+                    this[i] = this[this.length - 1];
+                    this.pop();
+                }
+            }
+        }
+        else {
+            throw "Array.prototype.remove 需要一个[function]参数";
+        }
+    };
+}
+else {
+    if (console) {
+        console.warn("function [Array.prototype.remove] 已经存在");
+    }
+}
+
