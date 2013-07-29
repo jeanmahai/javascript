5dd1199c23e6384d4feff51b0b26863674b97b0c
jean-home <mahai_1986@126.com>
2013-07-29 19:00:29 +0800

    jqui ext

---
+++ javascript/js/libs/jqext/jloading.1.0.0.js
@@ -0,0 +1,45 @@
+﻿(function () {
+
+    if (!$.loadingConfig) {
+        /*
+        url;
+        */
+        $.loadingConfig = function (cfg) {
+            $.data("__loding__config__", cfg);
+        };
+    }
+
+    $.fn.extend({
+        loading: function () {
+            var cfg = $.data("__loading__config__");
+            if (!cfg) cfg = { };
+
+            var dom_conver = $("<div></div>");
+            dom_conver.css({
+                width: $(window).width(),
+                height: $(window).height(),
+                position: "fixed",
+                top: 0,
+                left: 0,
+                opacity: 0.5,
+                backgroundColor: "white"
+            });
+            dom_conver.appendTo(document.body);
+
+            var dom_loading = $("<div></div>");
+            dom_loading.css({
+                position: "fixed",
+                background:"url("+cfg.url||""+")"
+            });
+
+            $(this).data("__loading__", { d1: dom_conver, d2: dom_loading });
+        },
+        loaded: function () {
+            var obj = $(this).data("__loading__");
+            if (obj) {
+                if (obj.d1) obj.d1.remove();
+                if (obj.d2) obj.d2.remove();
+            }
+        }
+    });
+})();
---
+++ javascript/js/libs/jqext/jquiext/jui.js
@@ -0,0 +1,76 @@
+﻿/// <reference path="../../jq/jquery-1.9.1.min.js" />
+/// <reference path="../../jq/jquery-ui-1.10.3.custom/js/jquery-ui-1.10.3.custom.js" />
+
+(function () {
+    if (!window.jui) window.jui = {};
+
+    var _dialog_container = $("<div></div>");
+
+
+    //#region alert
+    jui.alert = function (message, title, ok, alwayShow, ops) {
+        _dialog_container.text(message);
+        var btns = {
+            "确定": function () {
+                if (ok && typeof ok == "function") {
+                    ok();
+                }
+                $(this).dialog("close");
+            }
+        };
+        if (ops) {
+            ops.title = title || "系统信息";
+            ops.buttons = btns;
+        }
+        else {
+            ops = {
+                title: title || "系统信息",
+                modal: true,
+                buttons: btns
+            };
+        }
+        if (alwayShow == true) {
+            _dialog_container.clone().dialog(ops);
+        }
+        else {
+            _dialog_container.dialog(ops);
+        }
+    };
+    //#endregion
+
+
+    //#region confirm
+    jui.confirm = function (message, title, yes, no, alwayShow, ops) {
+        _dialog_container.text(message);
+        var btns = {
+            "确定": function () {
+                if (yes && typeof yes == "function") {
+                    yes();
+                }
+                $(this).dialog("close");
+            },
+            "取消": function () {
+                if (no && typeof no == "function") {
+                    no();
+                }
+                $(this).dialog("close");
+            }
+        };
+        if (ops) {
+            ops.title = title || "系统信息";
+            ops.buttons = btns;
+        } else {
+            ops = {
+                title: title || "系统信息",
+                modal: true,
+                buttons: btns
+            };
+        }
+        if (alwayShow == true) {
+            _dialog_container.clone().dialog(ops);
+        } else {
+            _dialog_container.dialog(ops);
+        }
+    };
+    //#endregion
+})();
