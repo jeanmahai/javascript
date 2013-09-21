4423adcf2101f08d702d61dc8f7095452a261587
jean-home <mahai_1986@126.com>
2013-09-21 15:58:41 +0800

    jposition

---
+++ javascript/js/libs/jqext/jposition.js
@@ -0,0 +1,15 @@
+﻿
+$.fn.extend({
+    /*
+    {
+    my:{x,y}center/left/right/top/bottom
+    at:{x,y}center/top/bottom/left/right
+    of:jquery object
+    mask:true/false
+    }
+    */
+    jposition: function (obj) {
+        if (!obj) return;
+        
+    }
+});
