5c764a17a4bf204f07656a61180a31f7320b2f49
jeanmahai <mahai_1986@126.com>
2013-11-25 17:39:49 +0800

    sdf

---
+++ javascript/PhotoPlayer/res/PhotoPlayer.js
@@ -0,0 +1,39 @@
+﻿/// <reference path="window.cancelRequestAnimationFrame.js" />
+/// <reference path="window.requestAnimationFrame.js" />
+
+
+function PhotoPlayer() {
+    this.downloadedImages = [];
+    this.imagesUrl = [];
+    this.cacheCount = 5;
+}
+
+PhotoPlayer.prototype = {
+    setImagesUrl: function (urls) {
+        this._urls = urls.split(",");
+    },
+    setCacheCount: function (count) {
+        this.cacheCount = count;
+    },
+    downloadImage: function () {
+        var me = this;
+        if (me.downloadedImages.length < me.cacheCount) {
+            var img = new Image();
+            img.onload = function () {
+                me.downloadedImages.push(this);
+                me.downloadImg();
+            };
+            img.onerror = function () {
+            };
+            img.src = me.imagesUrl.shift();
+        }
+        requestAnimationFrame(me.downloadImage);
+    },
+    startDownloadImages: function () {
+        var me = this;
+        if (me.downloadedImages.length < me.cacheCount) {
+            me.downloadImage();
+        }
+        requestAnimationFrame(me.startDownloadImages);
+    }
+};
