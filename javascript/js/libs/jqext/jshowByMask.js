fd3c7751bc474174ebfdca05a9009c13bed498f2
jeanmahai <mahai_1986@126.com>
2013-12-16 11:19:44 +0800

    add showByMask,placeholder

---
+++ javascript/js/libs/jqext/jshowByMask.js
@@ -0,0 +1,27 @@
+﻿/*
+带遮罩层的弹出层,弹出层的样式由className,控制,可以试一组className;
+如:className:mask  mask,mask-login
+*/
+$.fn.extend({
+    //className:mask  mask,mask-login
+    showByMask: function (className, container) {
+        var mask = $("<div></div>");
+        var classNames = className.split(",");
+        for (var i = 0; i < classNames.length; i += 1)
+            mask.addClass(classNames[i]);
+        
+        if (container) mask.appendTo(container);
+        else mask.appendTo(document.body);
+        
+        if ($(this).data("__mask__") && $(this).data("__mask__").remove) $(this).data("__mask__").remove();
+        
+        $(this).data("__mask__", mask);
+        $(this).show();
+    },
+    hideByMask: function () {
+        var mask = $(this).data("__mask__");
+        $(this).removeData("__mask__");
+        if (mask) mask.remove();
+        $(this).hide();
+    }
+});
---
+++ javascript/js/libs/tools/placeholder.js
@@ -0,0 +1,74 @@
+﻿/*
+用于兼容html5的新特性placeholder
+@html 是html代码片段,如果为null,则默认是document
+需要外部css的支持
+.placeholder{} 用于缺省文本的样式
+*/
+
+function initPlacholder(html) {
+    if ("placeholder" in document.createElement("input")) return;
+
+    var css = "placeholder";
+
+    var texts
+        ,passwords;
+    var textFilter = "input[type=text],area"
+        ,passwordFilter="input[type=password]";
+    if(html) {
+        texts = $(html).find(textFilter);
+        passwords = $(html).find(passwordFilter);
+    }
+    else {
+        texts = $(textFilter);
+        passwords = $(passwordFilter);
+    }
+    
+    if(texts) {
+        texts.each(function () {
+            var pl = $(this).attr("placeholder");
+            if (pl) {
+                $(this).val(pl).addClass(css);
+            }
+        });
+        texts.bind("focus", function () {
+            var val = $.trim($(this).val());
+            var pl = $(this).attr("placeholder");
+            if (val == pl) {
+                $(this).val("").removeClass("placeholder");
+            }
+        });
+        texts.bind("blur", function () {
+            var val = $.trim($(this).val());
+            var pl = $(this).attr("placeholder");
+            if (val == "") {
+                $(this).val(pl);
+                $(this).addClass("placeholder");
+            }
+        });
+    }
+    if(passwords) {
+        passwords.each(function () {
+            var pl = $(this).attr("placeholder");
+            if (pl) {
+                $(this).val(pl).addClass(css);
+            }
+        });
+        passwords.bind("focus", function () {
+            var val = $.trim($(this).val());
+            var pl = $(this).attr("placeholder");
+            if (val == pl) {
+                $(this).val("").removeClass("placeholder");
+                $(this).attr("type", "password");
+            }
+        });
+        passwords.bind("blur", function () {
+            var val = $.trim($(this).val());
+            var pl = $(this).attr("placeholder");
+            if (val == "") {
+                $(this).val(pl);
+                $(this).addClass("placeholder");
+                $(this).attr("type", "text");
+            }
+        });
+    }
+}
