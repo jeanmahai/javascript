1ea14510d1ecdbb0a3a9b11f34b83f0b735c05d3
jeanmahai <mahai_1986@126.com>
2013-12-23 14:58:13 +0800

    add from ajax

---
+++ javascript/js/test/jform/jform.js
@@ -0,0 +1,30 @@
+﻿/// <reference path="../../libs/jq/jquery-1.9.1.min.js" />
+$.fn.extend({
+    formatData: function () {
+        var data = {};
+        var val;
+        $(this).find("[name]").each(function () {
+            var self = $(this);
+            if (self.is("select")) {
+                val = self.find("option:selected").val();
+            }
+            else {
+                val = self.val();
+            }
+            data[self.attr("name")] = val;
+        });
+        return data;
+    },
+    request: function (cfg) {
+        if (!$(this).is("form")) return;
+        var self = $(this);
+        var params = {
+            dataType: "JSON"
+        };
+        $.extend(params, cfg);
+        params.type = self.attr("method");
+        params.url = self.attr("action");
+        params.data = self.formatData();
+        $.ajax(params);
+    }
+});
---
+++ javascript/js/test/jform/test.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace js.test.jform
+{
+    public partial class test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write(new JavaScriptSerializer().Serialize(new { Name = Request.QueryString["Name"] }));
+            Response.End();
+        }
+    }
+}
